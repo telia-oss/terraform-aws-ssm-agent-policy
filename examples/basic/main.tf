59e94d6960428a0254e3746d4dce86ae6263e170
itsdalmo <kristian@doingit.no>
2019-07-29 15:25:07 +0200

    Terraform 0.12upgrade

---
+++ examples/basic/main.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+provider "aws" {
+  version = ">= 2.17"
+  region  = var.region
+}
+
+data "aws_vpc" "main" {
+  default = true
+}
+
+data "aws_subnet_ids" "main" {
+  vpc_id = data.aws_vpc.main.id
+}
+
+data "aws_ami" "linux2" {
+  owners      = ["amazon"]
+  most_recent = true
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami*gp2"]
+  }
+}
+
+module "asg" {
+  source  = "telia-oss/asg/aws"
+  version = "3.0.1"
+
+  name_prefix  = var.name_prefix
+  vpc_id       = data.aws_vpc.main.id
+  subnet_ids   = data.aws_subnet_ids.main.ids
+  instance_ami = data.aws_ami.linux2.id
+
+  tags = {
+    terraform   = "True"
+    environment = "dev"
+  }
+}
+
+module "ssm_agent_policy" {
+  source = "../../"
+
+  name_prefix = var.name_prefix
+  role        = module.asg.role_name
+}
+
--- variables.tf
+++ variables.tf
@@ -3,13 +3,17 @@
 # ------------------------------------------------------------------------------
 variable "name_prefix" {
   description = "A prefix used for naming resources."
+  type        = string
 }
 
 variable "role" {
   description = "The IAM role to attach to the policy."
+  type        = string
 }
 
 variable "output_bucket" {
   description = "Name of a bucket where the SSM agent will be allowed to write command outputs."
+  type        = string
   default     = ""
 }
+
